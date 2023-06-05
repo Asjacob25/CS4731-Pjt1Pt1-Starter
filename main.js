dfb84016c3bbe45dee2bab46e598a1465112115d
Ashley Jacob <128179385+Asjacob25@users.noreply.github.com>
2023-06-04 22:52:16 -0400

    changes

--- main.js
+++ main.js
@@ -19,4 +19,38 @@ function main()
 
     //Set up the viewport
     gl.viewport( 0, 0, 400, 400);
+
+    var points = [];
+    points.push(vec4(0.5,-0.5,0.0,1.0));
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition")
+
+    gl.enableVertexAttribArray(vPosition);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0,0)
+    gl.drawArrays(gl.POINTS, 0, points.length)
+
+    //let form = document.getElementById("fileupload");
+    let inputElement = document.getElementById("files");
+    /*inputElement.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let fileElement = document.getElementById("files");
+        let fileReader = readTextFile(e);*/
+
+
+   // });
 }
+
+function renderFile(evt)
+{
+    e.preventDefault();
+
+    let fileElement = document.getElementById("files");
+    let fileReader = readTextFile(e);
+}
+
+
